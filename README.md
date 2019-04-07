5989c110474419fdc46e39e73ae81a25f815a940
jade <leeduyoung2002@gmail.com>
2019-04-07 14:49:05 +0900

    ok

---
+++ README.md
@@ -0,0 +1,11 @@
+# install
+
+npm i
+
+# run
+
+npm run dev
+
+# url
+
+open browser `localhost:8080`
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
     entry: {
-        'bundle': './src/App.tsx',
+        bundle: "./src/App.tsx"
     },
     output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, 'assets/js')
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "assets/js"),
+        publicPath: "assets/js"
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },    
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, "./"),
+        compress: true,
+        port: 8080,
+        inline: true,
+        hot: true,
+        open: true
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/
             },
             {
                 test: /\.s?css$/,
-                use: [
-                    "style-loader",
-                    "css-loader?url=false",
-                    "sass-loader"
-                ]
+                use: ["style-loader", "css-loader?url=false", "sass-loader"]
             }
         ]
     },
     node: {
-        fs: 'empty'
+        fs: "empty"
     }
 };
